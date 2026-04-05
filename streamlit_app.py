03fe2ef03e3e3c4da125a89d458f545c3496b947
Juan David Salguero Medina <98365000+jsalguero1@users.noreply.github.com>
2026-04-05 13:54:42 -0500

    Fix case sensitivity in watermelon API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,5 +39,5 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered! {name_on_order}!', icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/Watermelon](https://my.smoothiefroot.com/api/fruit/Watermelon)")  
 st.text(smoothiefroot_response)
